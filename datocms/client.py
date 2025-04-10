b7364a3157c44df868a956f8310eab40ebfc34dc
Rostyslav Bohomaz <rostyslav.db@gmail.com>
2025-04-10 13:54:12 +0300

    feat!: implement job polling and enhance iter pages methods

--- datocms/client.py
+++ datocms/client.py
@@ -2,27 +2,37 @@ from typing import (
     TYPE_CHECKING,
     Literal,
     Iterable,
+    AsyncIterable,
     Protocol,
     Generic,
     TypeVar,
     Generator,
+    AsyncGenerator,
     cast,
     Any,
-    Unpack,
     Optional,
     MutableMapping,
     Mapping,
 )
 from types import TracebackType
 from os import environ, PathLike
+from time import sleep as sync_sleep
+from asyncio import sleep as async_sleep
 
-from httpx import Auth, Request, Response, Client as _Client, USE_CLIENT_DEFAULT
+from httpx import (
+    Auth,
+    Request,
+    Response,
+    Client as _Client,
+    AsyncClient as _AsyncClient,
+    USE_CLIENT_DEFAULT,
+)
 
 if TYPE_CHECKING:
     from httpx._types import TimeoutTypes
 
     from .types.api import *
-    from .types.errors import ErrorAttributes, Error
+    from .types.errors import ErrorAttributes, Error, ErrorCodeType
     from .types.record import Record
     from .types.model import Model
     from .types.job import JobResult
@@ -36,9 +46,18 @@ __all__ = ["Client", "AsyncClient"]
 
 T = TypeVar("T")
 
+DEFAULT_RETRY_DELAY = 1.0
+DEFAULT_MAX_RETRIES = 10
+
 
 class GetPage(Protocol, Generic[T]):
-    def __call__(self, **kwargs: "Unpack[PageParams]") -> tuple[list[T], int]: ...
+    def __call__(self, limit: int, offset: int, **kwargs) -> tuple[list[T], int]: ...
+
+
+class GetPageAsync(Protocol, Generic[T]):
+    async def __call__(
+        self, limit: int, offset: int, **kwargs
+    ) -> tuple[list[T], int]: ...
 
 
 class DatoAuth(Auth):
@@ -63,6 +82,7 @@ class DatoError(Exception):
 
 class DatoGraphqlError(DatoError):
     def __init__(self, message: str, locations: list["ErrorLocation"], path: list[str]):
+        super().__init__()
         self.message = message
         self.locations = locations
         self.path = path
@@ -83,9 +103,12 @@ class DatoGraphqlError(DatoError):
 
 
 class DatoApiError(DatoError):
+    code: "ErrorCodeType"
+
     def __init__(
         self, code: str, doc_url: str, details: dict[str, Any], transient: bool
     ):
+        super().__init__()
         self.code = code
         self.doc_url = doc_url
         self.details = details
@@ -286,21 +309,6 @@ class BaseClient:
         return result["data"], result["meta"]["total_count"]
 
     @staticmethod
-    def _iter_paginated(
-        func: GetPage[T], page_size: int = 30, /, **kwargs
-    ) -> Generator[T, None, None]:
-        offset: int = 0
-        limit: int = page_size
-        total: int = 1
-
-        while offset < total:
-            items, total = func(limit=limit, offset=offset, **kwargs)
-            offset += len(items)
-
-            for item in items:
-                yield item
-
-    @staticmethod
     def _graphql_payload(
         query: str, variables: Mapping[str, Any] | None = None
     ) -> "PayloadGraphQL":
@@ -345,6 +353,20 @@ class Client(BaseClient):
         traceback: TracebackType | None = None,
     ):
         self._client.__exit__(exc_type, exc_value, traceback)
+
+    @staticmethod
+    def _iter_paginated(
+        func: GetPage[T], page_size: int = 30, /, **kwargs
+    ) -> Generator[T, None, None]:
+        offset: int = 0
+        total: int = 1
+
+        while offset < total:
+            items, total = func(limit=page_size, offset=offset, **kwargs)
+            offset += len(items)
+
+            for item in items:
+                yield item
 
     def execute(
         self,
@@ -410,6 +432,24 @@ class Client(BaseClient):
         data = cast("JobResult", self._handle_data_response(response))["attributes"]
         status, payload = data["status"], data["payload"]
         return status, payload["data"] if payload else None
+
+    def poll_job(
+        self,
+        id: str,
+        max_retries: int = DEFAULT_MAX_RETRIES,
+        retry_delay: float = DEFAULT_RETRY_DELAY,
+        timeout: "Optional[TimeoutTypes]" = None,
+    ):
+        for _ in range(max_retries):
+            try:
+                status, result = self.get_job_result(id, timeout=timeout)
+                if result is not None and status >= 200 and status < 300:
+                    return result
+            except DatoApiError as err:
+                if err.code != "NOT_FOUND":
+                    raise err
+            sync_sleep(retry_delay)
+        raise RuntimeError("Max retries exceeded")
 
     def list_models(self, *, timeout: "Optional[TimeoutTypes]" = None) -> list["Model"]:
         response = self._client.request(
@@ -506,7 +546,7 @@ class Client(BaseClient):
         assert result.get("type") == "upload_request"
         return result
 
-    def start_upload_job(
+    def create_upload_job(
         self,
         path: str,
         copyright: str | None = None,
@@ -542,6 +582,8 @@ class Client(BaseClient):
         notes: str | None = None,
         metadata: "Optional[Localized[Metadata]]" = None,
         tags: list[str] | None = None,
+        max_retries: int = DEFAULT_MAX_RETRIES,
+        retry_delay: float = DEFAULT_RETRY_DELAY,
     ):
         result = self.request_upload_permission(filename)
         url = result["attributes"]["url"]
@@ -553,13 +595,17 @@ class Client(BaseClient):
             "PUT", url, headers=headers, content=content, auth=None
         )
         response.raise_for_status()
-        return self.start_upload_job(
+        job = self.create_upload_job(
             result["id"],
             copyright=copyright,
             author=author,
             notes=notes,
             metadata=metadata,
             tags=tags,
+        )
+        sync_sleep(retry_delay)
+        return self.poll_job(
+            job["id"], max_retries=max_retries, retry_delay=retry_delay
         )
 
     def get_upload(self, id: str, timeout: "Optional[TimeoutTypes]" = None) -> "Upload":
@@ -710,4 +756,398 @@ class Client(BaseClient):
         *,
         tag: "TagType" = "manual",
     ) -> Generator["UploadTag", None, None]:
+        return self._iter_paginated(self.list_tags, page_size, tag=tag)
+
+
+class AsyncClient(BaseClient):
+    _client: _AsyncClient
+
+    def __init__(self, token: str | None = None):
+        super().__init__(token)
+        self._client = _AsyncClient(
+            auth=self._auth, base_url=self.base_url, follow_redirects=False
+        )
+
+    async def __aenter__(self):
+        await self._client.__aenter__()
+        return self
+
+    async def __aexit__(
+        self,
+        exc_type: type[BaseException] | None = None,
+        exc_value: Exception | None = None,
+        traceback: TracebackType | None = None,
+    ):
+        await self._client.__aexit__(exc_type, exc_value, traceback)
+
+    @staticmethod
+    async def _iter_paginated(
+        func: GetPageAsync[T], page_size: int = 30, /, **kwargs
+    ) -> AsyncGenerator[T, None]:
+        offset: int = 0
+        total: int = 1
+
+        while offset < total:
+            items, total = await func(limit=page_size, offset=offset, **kwargs)
+            offset += len(items)
+
+            for item in items:
+                yield item
+
+    async def execute(
+        self,
+        query: str,
+        variables: Mapping[str, Any] | None = None,
+        *,
+        environment: str | None = None,
+        include_drafts: bool = False,
+        exclude_invalid: bool | None = None,
+        timeout: "Optional[TimeoutTypes]" = None,
+    ) -> dict[str, Any] | None:
+        response = await self._client.request(
+            "POST",
+            self.graphql_url,
+            headers=self._graphql_headers(
+                environment=environment,
+                include_drafts=include_drafts,
+                exclude_invalid=exclude_invalid,
+            ),
+            json=self._graphql_payload(query, variables),
+            timeout=timeout or USE_CLIENT_DEFAULT,
+        )
+        return self._handle_graphql_response(response)
+
+    async def execute_from_file(
+        self,
+        path: PathLike,
+        variables: dict[str, Any] | None = None,
+        *,
+        environment: str | None = None,
+        include_drafts: bool = False,
+        exclude_invalid: bool | None = None,
+        timeout: "Optional[TimeoutTypes]" = None,
+    ) -> dict[str, Any] | None:
+        with open(path, "r") as fp:
+            return await self.execute(
+                fp.read(),
+                variables,
+                environment=environment,
+                include_drafts=include_drafts,
+                exclude_invalid=exclude_invalid,
+                timeout=timeout,
+            )
+
+    async def list_fields(
+        self, id: str, *, timeout: "Optional[TimeoutTypes]" = None
+    ) -> list:
+        response = await self._client.request(
+            "GET",
+            f"item-types/{id}/fields",
+            headers=self._api_headers,
+            timeout=timeout or USE_CLIENT_DEFAULT,
+        )
+        return self._handle_data_response(response)
+
+    async def get_job_result(
+        self, id: str, *, timeout: "Optional[TimeoutTypes]" = None
+    ) -> tuple[int, "Model"]:
+        response = await self._client.request(
+            "GET",
+            f"job-results/{id}",
+            headers=self._api_headers,
+            timeout=timeout or USE_CLIENT_DEFAULT,
+        )
+        data = cast("JobResult", self._handle_data_response(response))["attributes"]
+        status, payload = data["status"], data["payload"]
+        return status, payload["data"] if payload else None
+
+    async def poll_job(
+        self,
+        id: str,
+        max_retries: int = DEFAULT_MAX_RETRIES,
+        retry_delay: float = DEFAULT_RETRY_DELAY,
+        timeout: "Optional[TimeoutTypes]" = None,
+    ):
+        for _ in range(max_retries):
+            try:
+                status, result = await self.get_job_result(id, timeout=timeout)
+                if result is not None and status >= 200 and status < 300:
+                    return result
+            except DatoApiError as err:
+                if err.code != "NOT_FOUND":
+                    raise err
+            await async_sleep(retry_delay)
+        raise RuntimeError("Max retries exceeded")
+
+    async def list_models(
+        self, *, timeout: "Optional[TimeoutTypes]" = None
+    ) -> list["Model"]:
+        response = await self._client.request(
+            "GET",
+            "item-types",
+            headers=self._api_headers,
+            timeout=timeout or USE_CLIENT_DEFAULT,
+        )
+        return self._handle_data_response(response)
+
+    async def list_records(
+        self,
+        *,
+        nested: bool = False,
+        ids: Iterable[str] | None = None,
+        types: Iterable[str] | None = None,
+        query: str | None = None,
+        fields: Mapping[str, Mapping[str, str]] | None = None,
+        only_valid: bool = False,
+        locale: str | None = None,
+        limit: int | None = None,
+        offset: int | None = None,
+        order_by: str | None = None,
+        version: "Optional[Version]" = None,
+        timeout: "Optional[TimeoutTypes]" = None,
+    ) -> tuple[list["Record"], int]:
+        params = self._records_params(
+            nested=nested,
+            locale=locale,
+            order_by=order_by,
+            version=version,
+        )
+        self._page_params(params, limit=limit, offset=offset)
+        self._filter_params(
+            params,
+            ids=ids,
+            types=types,
+            query=query,
+            fields=fields,
+            only_valid=only_valid,
+        )
+
+        response = await self._client.request(
+            "GET",
+            "items",
+            params=params,
+            headers=self._api_headers,
+            timeout=timeout or USE_CLIENT_DEFAULT,
+        )
+        return self._handle_list_response(response)
+
+    def iter_records(
+        self,
+        page_size: int = 30,
+        *,
+        nested: bool = False,
+        ids: Iterable[str] | None = None,
+        types: Iterable[str] | None = None,
+        query: str | None = None,
+        fields: Mapping[str, Mapping[str, str]] | None = None,
+        only_valid: bool = False,
+        locale: str | None = None,
+        order_by: str | None = None,
+        version: "Optional[Version]" = None,
+        timeout: "Optional[TimeoutTypes]" = None,
+    ) -> AsyncGenerator["Record", None]:
+        return self._iter_paginated(
+            self.list_records,
+            page_size,
+            nested=nested,
+            ids=ids,
+            types=types,
+            query=query,
+            fields=fields,
+            only_valid=only_valid,
+            locale=locale,
+            order_by=order_by,
+            version=version,
+            timeout=timeout,
+        )
+
+    async def request_upload_permission(
+        self,
+        filename: str,
+        timeout: "Optional[TimeoutTypes]" = None,
+    ) -> "UploadPermission":
+        response = await self._client.post(
+            "upload-requests",
+            headers=self._upload_headers,
+            json=self._api_params("upload_request", filename=filename.lower()),
+            timeout=timeout or USE_CLIENT_DEFAULT,
+        )
+        result: "UploadPermission" = self._handle_data_response(response)
+        assert result.get("type") == "upload_request"
+        return result
+
+    async def create_upload_job(
+        self,
+        path: str,
+        copyright: str | None = None,
+        author: str | None = None,
+        notes: str | None = None,
+        metadata: "Optional[Localized[Metadata]]" = None,
+        tags: list[str] | None = None,
+        timeout: "Optional[TimeoutTypes]" = None,
+    ) -> "Job":
+        attributes = self._upload_params(
+            path=path,
+            copyright=copyright,
+            author=author,
+            notes=notes,
+            metadata=metadata,
+            tags=tags,
+        )
+        response = await self._client.post(
+            "uploads",
+            json=self._api_params("upload", **attributes),
+            headers=self._upload_headers,
+            timeout=timeout or USE_CLIENT_DEFAULT,
+        )
+        return self._handle_data_response(response)
+
+    async def create_upload(
+        self,
+        filename: str,
+        content: bytes | Iterable[bytes] | AsyncIterable[bytes],
+        content_type: str | None = None,
+        copyright: str | None = None,
+        author: str | None = None,
+        notes: str | None = None,
+        metadata: "Optional[Localized[Metadata]]" = None,
+        tags: list[str] | None = None,
+        max_retries: int = DEFAULT_MAX_RETRIES,
+        retry_delay: float = DEFAULT_RETRY_DELAY,
+    ):
+        result = await self.request_upload_permission(filename)
+        url = result["attributes"]["url"]
+        headers = result["attributes"]["request_headers"]
+        if content_type is not None:
+            headers["Content-Type"] = content_type
+
+        response = await self._client.request(
+            "PUT", url, headers=headers, content=content, auth=None
+        )
+        response.raise_for_status()
+        job = await self.create_upload_job(
+            result["id"],
+            copyright=copyright,
+            author=author,
+            notes=notes,
+            metadata=metadata,
+            tags=tags,
+        )
+        await async_sleep(retry_delay)
+        return await self.poll_job(
+            job["id"], max_retries=max_retries, retry_delay=retry_delay
+        )
+
+    async def get_upload(
+        self, id: str, timeout: "Optional[TimeoutTypes]" = None
+    ) -> "Upload":
+        response = await self._client.request(
+            "GET",
+            f"uploads/{id}",
+            headers=self._api_headers,
+            timeout=timeout or USE_CLIENT_DEFAULT,
+        )
+        return self._handle_data_response(response)
+
+    async def delete_upload(
+        self, id: str, timeout: "Optional[TimeoutTypes]" = None
+    ) -> "Upload":
+        response = await self._client.request(
+            "DELETE",
+            f"uploads/{id}",
+            headers=self._api_headers,
+            timeout=timeout or USE_CLIENT_DEFAULT,
+        )
+        return self._handle_data_response(response)
+
+    async def list_uploads(
+        self,
+        ids: Iterable[str] | None = None,
+        query: str | None = None,
+        fields: Mapping[str, Mapping[str, str]] | None = None,
+        order_by: str | None = None,
+        locale: str | None = None,
+        limit: int | None = None,
+        offset: int | None = None,
+    ) -> tuple[list["Upload"], int]:
+        params = self._items_params(locale=locale, order_by=order_by)
+        self._page_params(params, limit=limit, offset=offset)
+        self._filter_params(
+            params,
+            ids=ids,
+            query=query,
+            fields=fields,
+        )
+
+        response = await self._client.request(
+            "GET", "uploads", params=params, headers=self._api_headers
+        )
+        return self._handle_list_response(response)
+
+    def iter_uploads(
+        self,
+        page_size: int = 30,
+        *,
+        ids: Iterable[str] | None = None,
+        query: str | None = None,
+        fields: Mapping[str, Mapping[str, str]] | None = None,
+        order_by: str | None = None,
+        locale: str | None = None,
+    ) -> AsyncGenerator["Upload", None]:
+        return self._iter_paginated(
+            self.list_uploads,
+            page_size,
+            order_by=order_by,
+            locale=locale,
+            ids=ids,
+            query=query,
+            fields=fields,
+        )
+
+    async def get_referenced_records_by_upload(
+        self,
+        id: str,
+        nested: bool = False,
+        version: "Optional[AnyVersion]" = None,
+    ) -> list["Record"]:
+        response = await self._client.request(
+            "GET",
+            f"uploads/{id}/references",
+            headers=self._api_headers,
+            params=self._records_params(nested=nested, version=version),
+        )
+        return self._handle_data_response(response)
+
+    async def create_tag(self, name: str) -> str:
+        response = await self._client.request(
+            "POST",
+            "upload-tags",
+            headers=self._upload_headers,
+            json=self._api_params("upload_tag", name=name),
+        )
+        return self._handle_data_response(response)
+
+    async def list_tags(
+        self,
+        tag: "TagType" = "manual",
+        query: str | None = None,
+        offset: int | None = None,
+        limit: int | None = None,
+    ) -> tuple[list["UploadTag"], int]:
+        self._page_params(params := {}, offset=offset, limit=limit)
+        self._filter_params(params, query=query)
+        response = await self._client.request(
+            "GET",
+            self._tags_endpoint(tag),
+            headers=self._api_headers,
+            params=params,
+        )
+        return self._handle_list_response(response)
+
+    def iter_tags(
+        self,
+        page_size: int = 30,
+        *,
+        tag: "TagType" = "manual",
+    ) -> AsyncGenerator["UploadTag", None]:
         return self._iter_paginated(self.list_tags, page_size, tag=tag)
